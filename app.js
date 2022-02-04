f53776a6f07f07ef62787b5fcd0fff0f343d8f11
dmevola <dmevola@gmail.com>
2022-02-04 18:42:52 -0500

    all done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 
@@ -144,21 +144,18 @@ promptUser()
 .then(promptProject)
 .then(portfolioData => {
 
-const pageHTML = generatePage(portfolioData);
-
-  fs.writeFile('./dist/index.html', pageHTML, err => {
-    if (err) {
-      console.log(err);
-      return
-    }
-    console.log('Portfolio complete! Check out index.html to see the output!');
-
-    fs.copyFile('./src/style.css', './dist/style.css', err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('style sheet copied successfully');
-    });
-  });
+return generatePage(portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML)
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+  return copyFile();
+})
+.then(copyFileReponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err)
 });
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,1 +1,34 @@
 const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if theres an error, reject the promise and send the error to the Promise's catch method
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied'
+            });
+    });
+});
+};
+
+module.exports = { writeFile, copyFile };
